44e039f2a25a7ea6faa5da91ebb9ec4d53de848b
Paul AZOULAI <paul.azoulai@ensta-paristech.fr>
2018-06-29 16:52:47 +0200

    fixed the distance function

--- src_python3/df_exploration.py
+++ src_python3/df_exploration.py
@@ -14,14 +14,27 @@ def identical_words(L,M) :   # given two lists of words, computes how many words
                 count += 1
                 
     return count
-    
-df = pd.read_pickle("data.pkl")    
+# 
+# df = pd.read_pickle("data.pkl")    
 
 def get_line(df, id) :
     return df.loc[id,:]
     
 # print(get_line(df, "W05-0503-parscit.130908.json"))
-
-for file in list(df.index.values) :
-    print(list(get_line(df, file)))
+# article = list(df.index.values)[0]
+# 
+# for file in list(df.index.values) :
+# 
+# 
+# 
+# for file in list(df.index.values) :
+#     print(list(get_line(df, file)))
+    
+    
+def distance(L,M) :
+    assert len(L)==len(M)
+    return sum([L[i]>0 and M[i]>0 for i in range(len(L))])
+    
+print(distance([1,0,3],[1,1,0]))
+    
     
