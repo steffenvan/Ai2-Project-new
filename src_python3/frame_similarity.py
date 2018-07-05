fbb1308fa4f95890a4bfa9c73fdced86e08f9a37
Steffen Van <steffenvan@Steffens-MacBook-Pro.local>
2018-07-05 15:46:24 +0200

    Removed the code for the partial sentences related to a frame. Only contains code for comparisons with full sentences

--- src_python3/frame_similarity.py
+++ src_python3/frame_similarity.py
@@ -30,25 +30,8 @@ def element_similarity(L,M):
 
 # Returns a dictionary of the frames of a file.
 # Keys: Frames
-# Values: Corresponding text to the frame
-# def tokenize_relevant_frames(file):
-#     text_dict = {}
-#     for sentence in file:
-#         for frame in sentence["frames"]:
-#             if frame["target"]["name"] in df.columns:
-#                 result = extract_text(frame)
-#                 annot = " ".join(result["annot"])
-#                 nlp_tokens = nlp(annot)
-#                 text_dict.update({result["name"] : annot})
-#                 # print("Frame: %s, Target: %s" % (result["name"], result["target"]))
-#                 # print("Full text: ", nlp_tokens)
-#     return text_dict
-#
-# reference_result = tokenize_relevant_frames(reference_file)
-# print(reference_result)
+# Values: Full sentence in which the frames are found
 
-
-print(df.index[0])
 def extract_frame_sentence(file):
     sentence = ""
     frame_and_sentence = {}
@@ -56,23 +39,23 @@ def extract_frame_sentence(file):
         for frame in list_of_frames["frames"]:
             if frame["target"]["name"] in df.columns:
                 sentence = " ".join(list_of_frames["tokens"])
-                # tokens.append(full_sentence)
                 frame_and_sentence.update({frame["target"]["name"] : sentence})
+
     return frame_and_sentence
+
 ref_frame_and_sentence = extract_frame_sentence(reference_file)
 
-# Computes the similarity value between the common frames of the reference abstract
+# Computes the similarity value of the full sentence of the common frames of the reference abstract
 # and all the other abstracts one by one.
 # TODO: find a proper value for the threshold (elem_sim).
 
-# Dictionary for the paper and its similarity value for the reference paper.
+# Dictionary for the paper and its similarity value with the reference paper.
 paper_and_sim_val = {}
 tfidf_vectorizer = TfidfVectorizer()
 
 for abstract_id in all_other_abstracts:
     sim_frame_count = element_similarity(reference_frames, get_frames_count(df, abstract_id))
-    # total_sim_val = 0.0
-    total_sim_val_sentence = 0.0
+    total_sim_val = 0.0
     if sim_frame_count > 4 :
         print("\n****************************************************************\n")
         print(abstract_id, "\n")
@@ -82,32 +65,23 @@ for abstract_id in all_other_abstracts:
 
         # Find common frames between the reference and other abstracts.
         for key in ref_frame_and_sentence.keys() & temp_frame_and_sentence.keys():
-            # cos_sim = 0.0
-            cos_sim_sentence = 0.0
-            # if reference_result[key] and temp_result[key]:      # if frames for both papers contains a text
-                # tuple of text related to the frame
-                # frame_tuple     = (reference_result[key], temp_result[key])
-                # full text in which the frames appear
-            sentences_tuple = (ref_frame_and_sentence[key], temp_frame_and_sentence[key])
+            # Resetting similarity value for each frame
+            cos_sim = 0.0
 
-            # frame_matrix    = tfidf_vectorizer.fit_transform(frame_tuple)
+            sentences_tuple = (ref_frame_and_sentence[key], temp_frame_and_sentence[key])
             sentence_matrix = tfidf_vectorizer.fit_transform(sentences_tuple)
 
-            # cos_sim         = cosine_similarity(frame_matrix[0:1], frame_matrix)[0][1]
-            cos_sim_sentence = cosine_similarity(sentence_matrix[0:1], sentence_matrix)[0][1]
+            cos_sim = cosine_similarity(sentence_matrix[0:1], sentence_matrix)[0][1]
+            total_sim_val += cos_sim
 
-            # total_sim_val   += cos_sim
-            total_sim_val_sentence += cos_sim_sentence
-            paper_and_sim_val.update({abstract_id:total_sim_val_sentence})
-
+            paper_and_sim_val.update({abstract_id:total_sim_val})
 
             print("Similar frame: %s" % key)
-            print("Cosine similarity: %f" % cos_sim_sentence)
+            print("Cosine similarity: %f" % cos_sim)
             print("Reference text: %s" % ref_frame_and_sentence[key])
-            # print("Reference text: %s" % reference_result[key])
-            # print("Compared text: %s\n" % temp_result[key])
+
             print("Compared text: %s\n" % temp_frame_and_sentence[key])
-        print("Total value: %f" % total_sim_val_sentence)
+        print("Total value: %f" % total_sim_val)
 
 # Ranking the papers from highest to lowest
 sorted_values = sorted(paper_and_sim_val.items(), key=lambda x: x[1], reverse=True)
