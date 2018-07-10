96007242bdfafae1714e5f91a8d5d2f9d2ac6800
Steffen Van <steffenvan@Steffens-MacBook-Pro.local>
2018-07-10 14:08:52 +0200

    Giving the path to abstract texts

--- src_python3/frame_similarity.py
+++ src_python3/frame_similarity.py
@@ -99,6 +99,8 @@ def extract_frame_sentence(file):
 
 ref_frame_and_sentence = extract_frame_sentence(reference_file)
 
+format = "txt"
+
 # Computes the similarity value of the full sentence of the common frames of the reference abstract
 # and all the other abstracts one by one.
 # TODO: find a proper value for the threshold (elem_sim).
@@ -109,76 +111,78 @@ paper_and_dice_val = {}
 paper_and_jaccard_val = {}
 
 tfidf_vectorizer = TfidfVectorizer()
-
 for abstract_id in all_other_abstracts:
     sim_frame_count = element_similarity(reference_frames, get_frames_count(df, abstract_id))
     total_sim_val = 0.0
     total_dice_val = 0.0
     total_jaccard_val = 0.0
-    #total_sent_val = 0.0
-    if sim_frame_count > 4 :
-        print("\n****************************************************************\n")
-        print(abstract_id, "\n")
-        temp_abstract = json.load(open(json_path + str(abstract_id)))
-        # temp_result = tokenize_relevant_frames(temp_abstract)
-        temp_frame_and_sentence = extract_frame_sentence(temp_abstract)
-
-        # Find common frames between the reference and other abstracts.
-        for key in ref_frame_and_sentence.keys() & temp_frame_and_sentence.keys():
-            # Resetting similarity value for each frame
-            cos_sim = 0.0
-            dice_sim = 0.0
-            jaccard_sim = 0.0
-            #sent_sim = 0.0
-
-            #sentences_tuple = (ref_frame_and_sentence[key], temp_frame_and_sentence[key])
-            #sentence_matrix = tfidf_vectorizer.fit_transform(sentences_tuple)
-
-            # Cosine similarity
-            cos_sim = tfidf_vector_similarity(ref_frame_and_sentence[key], temp_frame_and_sentence[key])
-            total_sim_val += cos_sim
-            # dice similarity
-            dice_sim = dice_coefficient(ref_frame_and_sentence[key], temp_frame_and_sentence[key])
-            total_dice_val += dice_sim
-
-            # jaccard similarity
-            jaccard_sim = jaccard_similarity_coefficient(ref_frame_and_sentence[key], temp_frame_and_sentence[key])
-            total_jaccard_val += jaccard_sim
-
-            # other similarity
-            #info_content_norm = True
-            #sent_sim = similarity(ref_frame_and_sentence[key], temp_frame_and_sentence[key])
-            #total_sent_val += sent_sim
-
-            paper_and_cos_val.update({abstract_id:total_sim_val})
-            paper_and_dice_val.update({abstract_id:total_dice_val})
-            paper_and_jaccard_val.update({abstract_id:total_jaccard_val})
-
-            print("Similar frame: %s" % key)
-            print("Cosine similarity: %f" % cos_sim)
-            print("Dice coefficient: %f" % dice_sim)
-            print("Jaccard similarity: %f" % jaccard_sim)
-            #print("Sentence similarity: %f" % sent_sim)
-            print("Reference text: %s" % ref_frame_and_sentence[key])
-
-            print("Compared text: %s\n" % temp_frame_and_sentence[key])
-        print("Total cos value: %f" % total_sim_val)
-        print("Total dice val: %f" % total_dice_val)
-        print("Total jaccard val: %f" % total_jaccard_val)
-        #print("Total sentence val: %f" % total_sent_val)
-
-#Ranking the papers from highest to lowest
-sorted_cos          = sorted(paper_and_cos_val.items(), key=lambda x: x[1], reverse=True)
-sorted_dice         = sorted(paper_and_dice_val.items(), key=lambda x: x[1], reverse=True)
-sorted_jaccard      = sorted(paper_and_jaccard_val.items(), key=lambda x: x[1], reverse=True)
-best_paper_cos      = max(paper_and_cos_val.items(), key=operator.itemgetter(1))[0]
-best_paper_dice     = max(paper_and_dice_val.items(), key=operator.itemgetter(1))[0]
-best_paper_jaccard  = max(paper_and_jaccard_val.items(), key=operator.itemgetter(1))[0]
-
-print("\nPaper and Cos values:\n", sorted_cos)
-print("\nPaper and Dice values:\n", sorted_dice)
-print("\nPaper and Jaccard values:\n", sorted_jaccard)
-print("\nSimilar paper count:", len(paper_and_cos_val))
-print("\nMost similar paper using cosine:", best_paper_cos)
-print("\nMost similar paper using dice:", best_paper_dice)
-print("\nMost similar paper using jaccard:", best_paper_jaccard)
+    abstract_txt = os.path.join(abstract_id[:-4] + format)
+    print(parent)
+    # path_to_abs_txt = os.path.join(root, abstract_txt)
+    total_sent_val = 0.0
+#     if sim_frame_count > 4 :
+#         print("\n****************************************************************\n")
+#         print(abstract_id, "\n")
+#         temp_abstract = json.load(open(json_path + str(abstract_id)))
+#         # temp_result = tokenize_relevant_frames(temp_abstract)
+#         temp_frame_and_sentence = extract_frame_sentence(temp_abstract)
+#         tfidf_vectorizer(abstract_id)
+#         # Find common frames between the reference and other abstracts.
+#         for key in ref_frame_and_sentence.keys() & temp_frame_and_sentence.keys():
+#             # Resetting similarity value for each frame
+#             cos_sim = 0.0
+#             dice_sim = 0.0
+#             jaccard_sim = 0.0
+#             #sent_sim = 0.0
+#
+#             #sentences_tuple = (ref_frame_and_sentence[key], temp_frame_and_sentence[key])
+#             #sentence_matrix = tfidf_vectorizer.fit_transform(sentences_tuple)
+#
+#             # Cosine similarity
+#             cos_sim = tfidf_vector_similarity(ref_frame_and_sentence[key], temp_frame_and_sentence[key])
+#             total_sim_val += cos_sim
+#             # dice similarity
+#             dice_sim = dice_coefficient(ref_frame_and_sentence[key], temp_frame_and_sentence[key])
+#             total_dice_val += dice_sim
+#
+#             # jaccard similarity
+#             jaccard_sim = jaccard_similarity_coefficient(ref_frame_and_sentence[key], temp_frame_and_sentence[key])
+#             total_jaccard_val += jaccard_sim
+#
+#             # other similarity
+#             #info_content_norm = True
+#             #sent_sim = similarity(ref_frame_and_sentence[key], temp_frame_and_sentence[key])
+#             #total_sent_val += sent_sim
+#
+#             paper_and_cos_val.update({abstract_id:total_sim_val})
+#             paper_and_dice_val.update({abstract_id:total_dice_val})
+#             paper_and_jaccard_val.update({abstract_id:total_jaccard_val})
+#
+#             print("Similar frame: %s" % key)
+#             print("Cosine similarity: %f" % cos_sim)
+#             print("Dice coefficient: %f" % dice_sim)
+#             print("Jaccard similarity: %f" % jaccard_sim)
+#             #print("Sentence similarity: %f" % sent_sim)
+#             print("Reference text: %s" % ref_frame_and_sentence[key])
+#
+#             print("Compared text: %s\n" % temp_frame_and_sentence[key])
+#         print("Total cos value: %f" % total_sim_val)
+#         print("Total dice val: %f" % total_dice_val)
+#         print("Total jaccard val: %f" % total_jaccard_val)
+#         #print("Total sentence val: %f" % total_sent_val)
+#
+# #Ranking the papers from highest to lowest
+# sorted_cos          = sorted(paper_and_cos_val.items(), key=lambda x: x[1], reverse=True)
+# sorted_dice         = sorted(paper_and_dice_val.items(), key=lambda x: x[1], reverse=True)
+# sorted_jaccard      = sorted(paper_and_jaccard_val.items(), key=lambda x: x[1], reverse=True)
+# best_paper_cos      = max(paper_and_cos_val.items(), key=operator.itemgetter(1))[0]
+# best_paper_dice     = max(paper_and_dice_val.items(), key=operator.itemgetter(1))[0]
+# best_paper_jaccard  = max(paper_and_jaccard_val.items(), key=operator.itemgetter(1))[0]
+#
+# print("\nPaper and Cos values:\n", sorted_cos)
+# print("\nPaper and Dice values:\n", sorted_dice)
+# print("\nPaper and Jaccard values:\n", sorted_jaccard)
+# print("\nSimilar paper count:", len(paper_and_cos_val))
+# print("\nMost similar paper using cosine:", best_paper_cos)
+# print("\nMost similar paper using dice:", best_paper_dice)
+# print("\nMost similar paper using jaccard:", best_paper_jaccard)
