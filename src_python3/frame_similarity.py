ddbfdf8480a39848209f9a698783d00da8431c84
Paul AZOULAI <paul.azoulai@ensta-paristech.fr>
2018-07-11 22:53:03 +0200

    added some cool stuff, like the vect_functions.py

--- src_python3/frame_similarity.py
+++ src_python3/frame_similarity.py
@@ -6,19 +6,19 @@ from extraction import *
 from path import *
 import pandas as pd
 import operator
-
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 from similarity_utility import *
 
 # Setting the correct paths
-json_path    = os.path.join(parent, "json_abs/")
 data_path    = os.path.join(parent, "data/")
-abs_txt_path = os.path.join(data_path, "txt_abs/")
+json_path    = os.path.join(data_path, "json/")
+txt_path = os.path.join(data_path, "txt/")
 
 # File loading utilities
 def get_document_text(document_id):
     format = "txt"
-    file_path = os.path.join(abs_txt_path, str(document_id[:-4]) + format)
+    file_path = os.path.join(txt_path, str(document_id[:-4]) + format)
     content = Path(file_path).read_text()
     return content
 
@@ -39,9 +39,7 @@ paper_and_jaccard_val = {}
 paper_and_wm_val = {}
 paper_and_embeddings_cos_val = {}
 
-def tfidf_vectorize_document(document):
-    # n highest tfidf valued words.
-    top_n_words = 10
+def tfidf_vectorize_document(document, topn = 10):
 
     vectorizer = TfidfVectorizer(stop_words='english')
     vectorized_abs = vectorizer.fit_transform([document])
@@ -51,11 +49,11 @@ def tfidf_vectorize_document(document):
     # Loop through the text in the document
     for i in range(vectorized_abs.shape[0]):
         full_text = vectorized_abs.getrow(i).toarray().ravel()
-        sorted_values = np.argsort(full_text)[::-1][:top_n_words]
+        sorted_values = np.argsort(full_text)[::-1][:topn]
 
         for word, tfidf in zip(words[sorted_values], full_text[sorted_values]):
             word_tfidf_val.update({word:tfidf})
-            print("%s - %f" %(word, tfidf))
+            # print("%s - %f" %(word, tfidf))
 
     return word_tfidf_val
 
@@ -65,11 +63,11 @@ def tfidf_vectorize_document(document):
 
 if  __name__ == "__main__" :
     df                     = load_dataframe()
-    reference_file         = json.load(open(json_path + str(df.index[0])))
+    # reference_file         = json.load(open(json_path + str(df.index[0])))
     all_other_abstracts    = df.index[1:]
     ref_content            = get_document_text(df.index[0])
     reference_frames       = get_frames_count(df.index[0])
-    ref_frame_and_sentence = extract_frame_sentence(reference_file)
+    ref_frame_and_sentence = extract_frame_sentence(os.path.join(json_path, str(df.index[0])))
 
     for abstract_id in all_other_abstracts:
         current_frame_count = get_frames_count(abstract_id)
@@ -85,8 +83,8 @@ if  __name__ == "__main__" :
             print("\n****************************************************************\n")
             print(abstract_id, "\n")
 
-            temp_abstract           = json.load(open(json_path + str(abstract_id)))
-            temp_frame_and_sentence = extract_frame_sentence(temp_abstract)
+            # temp_abstract           = json.load(open(json_path + str(abstract_id)))
+            temp_frame_and_sentence = extract_frame_sentence(os.path.join(json_path + str(abstract_id)))
 
             doc_text                = get_document_text(abstract_id)
             weighted_document       = tfidf_vectorize_document(doc_text)
@@ -95,81 +93,83 @@ if  __name__ == "__main__" :
             print("\n")
             # Find common frames between the reference and other abstracts.
             for key in ref_frame_and_sentence.keys() & temp_frame_and_sentence.keys():
+                for sentence in temp_frame_and_sentence[key] :
+                    print(sentence)
                 # Resetting similarity value for each frame
-                cos_sim       = 0.0
-                dice_sim      = 0.0
-                jaccard_sim   = 0.0
-                wm_sim        = 0.0
-                embed_cos_sim = 0.0
-
-                # Cosine similarity
-                cos_sim            = tfidf_vector_similarity(ref_frame_and_sentence[key],
-                                                             temp_frame_and_sentence[key])
-                total_sim_val     += cos_sim
-
-                # dice similarity
-                dice_sim           = dice_coefficient(ref_frame_and_sentence[key],
-                                                      temp_frame_and_sentence[key])
-                total_dice_val    += dice_sim
-
-                # jaccard similarity
-                jaccard_sim        = jaccard_sim_coefficient(ref_frame_and_sentence[key],
-                                                             temp_frame_and_sentence[key])
-                total_jaccard_val += jaccard_sim
-
-                # Word mover distance
-                wm_sim             = WMD(ref_frame_and_sentence[key],
-                                         temp_frame_and_sentence[key])
-                total_wm_val      += wm_sim
-
-                # cosine similarity between word embedding vectors
-                embed_cos_sim      = embedding_sentence_similarity(ref_frame_and_sentence[key],
-                                                                   temp_frame_and_sentence[key])
-                total_embed_val   += embed_cos_sim
-
-                paper_and_cos_val.update({abstract_id:total_sim_val})
-                paper_and_dice_val.update({abstract_id:total_dice_val})
-                paper_and_jaccard_val.update({abstract_id:total_jaccard_val})
-                paper_and_wm_val.update({abstract_id:total_wm_val})
-                paper_and_embeddings_cos_val.update({abstract_id:total_embed_val})
-
-                print("Similar frame: %s" % key)
-                print("Cosine similarity: %f" % cos_sim)
-                print("Dice coefficient: %f" % dice_sim)
-                print("Jaccard similarity: %f" % jaccard_sim)
-                print("Word Mover similarity: %f" % wm_sim)
-                print("Embeddings cosine similarity: %f" % embed_cos_sim)
-                print("Reference text: %s" % ref_frame_and_sentence[key])
-
-                print("Compared text: %s\n" % temp_frame_and_sentence[key])
-            print("Total cos value: %f" % total_sim_val)
-            print("Total dice val: %f" % total_dice_val)
-            print("Total jaccard val: %f" % total_jaccard_val)
-            print("Total Word Mover val: %f" % total_wm_val)
-            print("Total embeddings cos value: %f" % total_embed_val)
-            #print("Total sentence val: %f" % total_sent_val)
-
-    #Ranking the papers from highest to lowest
-    sorted_cos          = sorted(paper_and_cos_val.items(), key=lambda x: x[1], reverse=True)
-    sorted_dice         = sorted(paper_and_dice_val.items(), key=lambda x: x[1], reverse=True)
-    sorted_jaccard      = sorted(paper_and_jaccard_val.items(), key=lambda x: x[1], reverse=True)
-    sorted_wm           = sorted(paper_and_wm_val.items(), key=lambda x: x[1], reverse=True)
-    sorted_emb_cos      = sorted(paper_and_embeddings_cos_val.items(), key=lambda x: x[1], reverse=True)
-
-    best_paper_cos      = max(paper_and_cos_val.items(), key=operator.itemgetter(1))[0]
-    best_paper_dice     = max(paper_and_dice_val.items(), key=operator.itemgetter(1))[0]
-    best_paper_jaccard  = max(paper_and_jaccard_val.items(), key=operator.itemgetter(1))[0]
-    best_paper_wm       = max(paper_and_wm_val.items(), key=operator.itemgetter(1))[0]
-    best_paper_emb_cos  = max(paper_and_embeddings_cos_val.items(), key=operator.itemgetter(1))[0]
-
-    print("\nPaper and Cos values:\n", sorted_cos)
-    print("\nPaper and Dice values:\n", sorted_dice)
-    print("\nPaper and Jaccard values:\n", sorted_jaccard)
-    print("\nPaper and word mover values:\n", sorted_wm)
-    print("\nPaper and embeddings cosine similarity values:\n", sorted_emb_cos)
-    print("\nTotal similar papers:", len(paper_and_cos_val))
-    print("\nMost similar paper using cosine:", best_paper_cos)
-    print("\nMost similar paper using dice:", best_paper_dice)
-    print("\nMost similar paper using jaccard:", best_paper_jaccard)
-    print("\nMost similar paper using word mover:", best_paper_wm)
-    print("\nMost similar paper using embeddings and cosine similarity:", best_paper_emb_cos)
+    #             cos_sim       = 0.0
+    #             dice_sim      = 0.0
+    #             jaccard_sim   = 0.0
+    #             wm_sim        = 0.0
+    #             embed_cos_sim = 0.0
+    # 
+    #             # Cosine similarity
+    #             cos_sim            = tfidf_vector_similarity(ref_frame_and_sentence[key],
+    #                                                          temp_frame_and_sentence[key])
+    #             total_sim_val     += cos_sim
+    # 
+    #             # dice similarity
+    #             dice_sim           = dice_coefficient(ref_frame_and_sentence[key],
+    #                                                   temp_frame_and_sentence[key])
+    #             total_dice_val    += dice_sim
+    # 
+    #             # jaccard similarity
+    #             jaccard_sim        = jaccard_sim_coefficient(ref_frame_and_sentence[key],
+    #                                                          temp_frame_and_sentence[key])
+    #             total_jaccard_val += jaccard_sim
+    # 
+    #             # Word mover distance
+    #             wm_sim             = WMD(ref_frame_and_sentence[key],
+    #                                      temp_frame_and_sentence[key])
+    #             total_wm_val      += wm_sim
+    # 
+    #             # cosine similarity between word embedding vectors
+    #             embed_cos_sim      = embedding_sentence_similarity(ref_frame_and_sentence[key],
+    #                                                                temp_frame_and_sentence[key])
+    #             total_embed_val   += embed_cos_sim
+    # 
+    #             paper_and_cos_val.update({abstract_id:total_sim_val})
+    #             paper_and_dice_val.update({abstract_id:total_dice_val})
+    #             paper_and_jaccard_val.update({abstract_id:total_jaccard_val})
+    #             paper_and_wm_val.update({abstract_id:total_wm_val})
+    #             paper_and_embeddings_cos_val.update({abstract_id:total_embed_val})
+    # 
+    #             print("Similar frame: %s" % key)
+    #             print("Cosine similarity: %f" % cos_sim)
+    #             print("Dice coefficient: %f" % dice_sim)
+    #             print("Jaccard similarity: %f" % jaccard_sim)
+    #             print("Word Mover similarity: %f" % wm_sim)
+    #             print("Embeddings cosine similarity: %f" % embed_cos_sim)
+    #             print("Reference text: %s" % ref_frame_and_sentence[key])
+    # 
+    #             print("Compared text: %s\n" % temp_frame_and_sentence[key])
+    #         print("Total cos value: %f" % total_sim_val)
+    #         print("Total dice val: %f" % total_dice_val)
+    #         print("Total jaccard val: %f" % total_jaccard_val)
+    #         print("Total Word Mover val: %f" % total_wm_val)
+    #         print("Total embeddings cos value: %f" % total_embed_val)
+    #         #print("Total sentence val: %f" % total_sent_val)
+    # 
+    # #Ranking the papers from highest to lowest
+    # sorted_cos          = sorted(paper_and_cos_val.items(), key=lambda x: x[1], reverse=True)
+    # sorted_dice         = sorted(paper_and_dice_val.items(), key=lambda x: x[1], reverse=True)
+    # sorted_jaccard      = sorted(paper_and_jaccard_val.items(), key=lambda x: x[1], reverse=True)
+    # sorted_wm           = sorted(paper_and_wm_val.items(), key=lambda x: x[1], reverse=True)
+    # sorted_emb_cos      = sorted(paper_and_embeddings_cos_val.items(), key=lambda x: x[1], reverse=True)
+    # 
+    # best_paper_cos      = max(paper_and_cos_val.items(), key=operator.itemgetter(1))[0]
+    # best_paper_dice     = max(paper_and_dice_val.items(), key=operator.itemgetter(1))[0]
+    # best_paper_jaccard  = max(paper_and_jaccard_val.items(), key=operator.itemgetter(1))[0]
+    # best_paper_wm       = max(paper_and_wm_val.items(), key=operator.itemgetter(1))[0]
+    # best_paper_emb_cos  = max(paper_and_embeddings_cos_val.items(), key=operator.itemgetter(1))[0]
+    # 
+    # print("\nPaper and Cos values:\n", sorted_cos)
+    # print("\nPaper and Dice values:\n", sorted_dice)
+    # print("\nPaper and Jaccard values:\n", sorted_jaccard)
+    # print("\nPaper and word mover values:\n", sorted_wm)
+    # print("\nPaper and embeddings cosine similarity values:\n", sorted_emb_cos)
+    # print("\nTotal similar papers:", len(paper_and_cos_val))
+    # print("\nMost similar paper using cosine:", best_paper_cos)
+    # print("\nMost similar paper using dice:", best_paper_dice)
+    # print("\nMost similar paper using jaccard:", best_paper_jaccard)
+    # print("\nMost similar paper using word mover:", best_paper_wm)
+    # print("\nMost similar paper using embeddings and cosine similarity:", best_paper_emb_cos)
--- src_python3/test.py
+++ src_python3/test.py
@@ -1,24 +1,57 @@
-from json import *
 from extraction import *
-from df_exploration import *
-from build_matrix2 import *
+from path import data_path
+import os
+from build_matrix import *
 import pandas as pd
-from path import *
-import pickle
+from similarity_utility import *
+from frame_similarity import get_document_text, tfidf_vectorize_document
+from vect_functions import sentence_vec, cosine_vec
 
-json_path = os.path.join(parent + "/json_abs/")
+json_path = os.path.join(data_path, "json/")
+
+df = load_dataframe()
+example_frame = "Cause_to_make_progress"
+ref = df.index[8]
+candidates = most_similar(ref, 50, df)
 
 
-L = pickle.load(open("out.pkl","rb"))
+sentences_ref = []
+doc_text                = get_document_text(ref)
+weighted_document       = tfidf_vectorize_document(doc_text, 20)
+important_words = weighted_document.keys()
+doc_frames = extract_frame_sentence(os.path.join(json_path, candidate), df)
+for sentence in doc_frames[example_frame] :
+        if sum([word.lower() in sentence.lower() for word in important_words]) > 6 :
+            sentences_ref.append(sentence)
 
-df = pd.read_pickle("data.pkl")
+docs = []
 
-i = 0
+for candidate in candidates[:6] :
+    sentences = []
+    doc_text                = get_document_text(candidate)
+    weighted_document       = tfidf_vectorize_document(doc_text, 20)
+    important_words = weighted_document.keys()
+    doc_frames = extract_frame_sentence(os.path.join(json_path, candidate), df)
+    # print(doc_frames)
+    # for frame in doc_frames.keys() :
+    #     for sentence in doc_frames[frame] :
+    #             if sum([word.lower() in sentence.lower() for word in important_words]) > 6 :
+    #                 print(sentence)
+    #                 print("\n")
+    for sentence in doc_frames[example_frame] :
+            if sum([word.lower() in sentence.lower() for word in important_words]) > 6 :
+                sentences.append(sentence)
+                print(sentence)
+                print("\n")
+    docs.append(sentences)
 
-for id in df.index :
-    print(get_line(df, id))
-    print(L[i])
-    i += 1
+max = 0
+
+for doc in docs :
+    for sentence in doc :
+        vect_doc = 
+        for sentence_ref in sentences_ref :
+            
     
     
 
