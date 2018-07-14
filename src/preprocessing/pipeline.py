5dd84a01ee6c6268beda7bd07886964f8ade9027
Steffen Van <steffenvan@Steffens-MacBook-Pro.local>
2018-07-14 23:32:15 +0200

    Now setting the correct path

--- src/preprocessing/pipeline.py
+++ src/preprocessing/pipeline.py
@@ -3,31 +3,32 @@ import os
 from preprocess_functions import *
 import sys
 from pathlib import Path
+# Creating path to the path.py file
 curr_file = (os.path.join(os.getcwd(), sys.argv[0]))
-src_path = Path(curr_file).parent.parent
-(sys.path.append(str(Path(curr_file).parent.parent)))
-
+sys.path.append(str(Path(curr_file).parent.parent))
 from path import *
 
-src = correct_path(root, str(Path.cwd()))
-print(root)
-print("current is:", Path.cwd())
-print("data folder is:", src)
-# if "src/preprocessing" or "src/visualization" in Path.cwd():
-#     root = Path.cwd().parent.parent
-# else:
-#     root = Path.cwd().parent
+
+src = correct_path(root)
+# print("\npipeline file:\n")
+# print("current is:", Path.cwd())
+# print("data folder is:", src)
 
 """
 to add new xml files to the database, simply drag them to data/xml/ and then run this script
 """
+
 print("data path is:", data_path)
 
-assert os.path.isfile(os.path.join(data_path, "corpus_words.txt")), "please add corpus_words.txt in the data folder"
+print("xml path is: ", xml_test_path)
+
+corpus_words_path = data_path.joinpath("corpus_words.txt")
+
+assert Path.is_file(corpus_words_path), "please add corpus_words.txt in the data folder"
 
 L = []
 
-for filename in os.listdir(xml_test_path) :
+for filename in os.listdir(xml_test_path):
     if filename.endswith(".xml") :
         print(filename)
         L.append(Article(filename[:-4]))
