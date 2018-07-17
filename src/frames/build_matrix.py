50699c8a1421d03a5b17a50ec82d82512cd32658
Steffen Van <steffenvan@Steffens-MacBook-Pro.local>
2018-07-17 22:51:44 +0200

    Cleanup and renaming

--- src/frames/build_matrix.py
+++ src/frames/build_matrix.py
@@ -8,12 +8,10 @@ sys.path.append(str(Path(curr_file).parents[1]))
 
 """
 This script allows the creation of two files :
-- a data frame, stored as data.pkl, containing for each files the number of occurences of the frames specified in the frames_to_keep list specified below
+- a data frame, stored as data.pkl, containing for each files the number of
+- occurences of the frames specified in the frames_to_keep list specified below
 - a list of dictionnaries, stored as "out.pkl". Each entry of the list is a dictionnary, with a
-
 """
-
-## TODO: Need to refactor the file to be able to import the files from path file correctly!
 
 frames_to_keep = ['Causation','Increment', 'Means', 'Aggregate','Relational_quantity', 'Evidence','Assessing','Inclusion','Usefulness','Reasoning', 'Cause_to_make_progress','Importance','Desirability', 'Evaluative_comparison', 'Performing_arts', 'Change_position_on_a_scale', 'Trust', 'Position_on_a_scale', 'Predicament', 'Supply', 'Accomplishment']
 
@@ -21,28 +19,28 @@ def get_frame(sentence):
     for frame in sentence["frames"]:
         return frame
 
-def open_json(id):
-    path_to_json = json_train_path.joinpath(id)
+def open_json(filename):
+    path_to_json = json_train_path.joinpath(filename)
     with open(path_to_json) as json_file:
         json_data = json.loads(json_file.read())
     return json_data
 
-def is_valid(id):
-    json_file = json_train_path.joinpath(id)
+def is_valid(filename):
+    json_file = json_train_path.joinpath(filename)
     if json_file.exists():
         try:
             return open_json(json_file)
         except:
-            print("Invalid json file: ", id)
+            print("Invalid json file: ", filename)
             return False
 
-def create_frame_content(file_id, data_frame):
+def create_frame_content(filename, data_frame):
     frames_text = []
-    d = {} ##
-    valid_file = is_valid(file_id)
+    d           = {} ##
+    valid_file  = is_valid(filename)
 
     if valid_file:
-        print(file_id + " open")
+        print(filename + " open")
 
         for sentence in valid_file:
             temp_frame = get_frame(sentence)
@@ -52,36 +50,37 @@ def create_frame_content(file_id, data_frame):
 
                 if temp_frame not in d.keys():
                     d[frame] = []
-                    test = extract_text(sentence)
+                    test     = extract_text(sentence)
                     d.update({frame:test})
     else:
-        os.remove(json_train_path.joinpath(file_id))
+        os.remove(json_train_path.joinpath(filename))
     frames_text.append(d)
+
     return frames_text
 
-def list_of_files(path_to_files, extension):
-    folder = os.listdir(path_to_files)
-    ids = [filename for filename in folder if filename.endswith(extension)]
-    return ids
+def create_file_list(path_to_files, extension):
+    folder            = os.listdir(path_to_files)
+    list_of_filenames = [filename for filename in folder if filename.endswith(extension)]
+    return list_of_filenames
 
 def create_pickle(list_of_frames, path, pkl_filename):
     frames_text_path = path.joinpath(pkl_filename)
-    output_file = open(frames_text_path, "wb+")
+    output_file      = open(frames_text_path, "wb+")
+    result           = pickle.dump(list_of_frames, output_file)
     print(output_file)
-    result = pickle.dump(list_of_frames, output_file)
     output_file.close()
     return result
 
 def data_frame_init(important_frames, rows):
-    columns = ["ID"] + important_frames
-    data_frame = pd.DataFrame(columns = columns)
+    columns          = ["ID"] + important_frames
+    data_frame       = pd.DataFrame(columns = columns)
     data_frame.fillna(value = 0, inplace = True)
     data_frame["ID"] = rows
     print(data_frame["ID"])
     return data_frame
 
-def build_matrix() :
-    filenames        = list_of_files(json_train_path, "json")
+def build_matrix():
+    filenames        = create_file_list(json_train_path, "json")
     total_file_count = len(filenames)
     df               = data_frame_init(frames_to_keep, filenames)
     count            = 0
@@ -99,9 +98,6 @@ def build_matrix() :
 
 build_matrix()
 
-
-#
-# #
 # # def build_abs_matrix() :                    # doesn't work for now because of abstract_json()
 # #     columns = ["ID"] + frames_to_keep
 # #     df = pd.DataFrame(columns = columns)
@@ -135,4 +131,3 @@ build_matrix()
 # #     output_file = open(os.path.join(data_path,"frames_text_abs.pkl"),"wb+")
 # #     pickle.dump(frames_text, output_file)
 # #     output_file.close()
-#
