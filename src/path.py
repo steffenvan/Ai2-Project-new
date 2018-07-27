f9645efc3178c6876b30fb7249a28736045b1242
Paul AZOULAI <paul.azoulai@ensta-paristech.fr>
2018-07-27 11:16:33 +0200

    added a function to generate the path to a file, given his id

--- src/path.py
+++ src/path.py
@@ -24,17 +24,15 @@ parent           = Path.resolve(real_root.joinpath(real_root.parent))
 data_path        = parent.joinpath("data/")
 run_semafor_path = parent.joinpath("semafor/bin/runSemafor.sh")
 
-print("Root folder:            ", real_root)
-print("Parent of project path: ", parent)
-print("Semafor path:           ", run_semafor_path)
+# print("Root folder:            ", real_root)
+# print("Parent of project path: ", parent)
+# print("Semafor path:           ", run_semafor_path)
 
 # train data
-
-train_path      = data_path.joinpath(data_path, "train/")
-
-json_train_path = train_path.joinpath(train_path, "json/")
-txt_train_path  = train_path.joinpath(train_path, "txt/")
-
+train_path      = data_path.joinpath("train/")
+json_train_path = train_path.joinpath("json/")
+txt_train_path  = train_path.joinpath("txt/")
+path_to_model = train_path.joinpath("myfullmodel.gsm")
 # test data
 test_path      = data_path.joinpath("test/")
 
@@ -43,7 +41,29 @@ txt_test_path  = test_path.joinpath("txt/")
 xml_test_path  = test_path.joinpath("xml/")
 rej_test_path  = test_path.joinpath("rejected_xml/")
 
-print(json_test_path)
-print(txt_test_path)
-print(xml_test_path)
-print(rej_test_path)
+# print(json_test_path)
+# print(txt_test_path)
+# print(xml_test_path)
+# print(rej_test_path)
+
+# the following function can take filenames (with or without paths) or just doc_id as input, and gives the absolute path to the txt or json file as an output
+
+def get_path(doc_id, json = True, train = True) :
+    doc_id = os.path.basename(doc_id)
+    if doc_id.endswith(".txt") :
+        doc_id = doc_id[:-4]
+    if doc_id.endswith(".json") :
+        doc_id = doc_id[:-5]
+    if json :
+        filename = doc_id + ".json"
+        if train :
+            path = os.path.join(json_train_path, filename)
+        else :
+            path = os.path.join(json_test_path, filename)
+    else :
+        filename = doc_id + ".txt"
+        if train :
+            path = os.path.join(txt_train_path, filename)
+        else : 
+            path = os.path.join(txt_test_path, filename)
+    return Path(path)
