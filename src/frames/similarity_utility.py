ae9ca261e56e717b47cc4e45b957cbbce9fafbba
Paul AZOULAI <paul.azoulai@ensta-paristech.fr>
2018-07-27 11:14:55 +0200

    added similarity functions for tfidf

--- src/frames/similarity_utility.py
+++ src/frames/similarity_utility.py
@@ -17,12 +17,16 @@ def get_document_text(document_id):
     return content
 
 # Load the dataframe for easy use.
-def load_dataframe(file = data_path.joinpath("data.pkl")) :
+def load_dataframe(file = data_path.joinpath("train_data.pkl")) :
+    return (pd.read_pickle(file))
+
+def load_tfidf_dataframe(file = data_path.joinpath("train_tfidf.pkl")) :
     return (pd.read_pickle(file))
 
 # Also adding a weight the important words.
 # TODO: experiment with the weight of the important words
 def sentence_vectorize(sentence, important_words, model) :
+    # print("TESTTTTTT")
     vec = 100*[0]
     l = 0
     for word in sentence.split(" ") :
@@ -30,15 +34,23 @@ def sentence_vectorize(sentence, important_words, model) :
             vec += model[word]*(1 + 0.5*int(word in important_words))
             l += 1 + 0.5 * int(word in important_words)
     if l == 0 :
+        
         return 100*[0]
+    
     return [elt/l for elt in vec]
 
 # Computes the number of a particular frame in a document.
-def get_frames_count(id, df = pd.DataFrame()) :
-    if len(df) == 0 :
-        df = load_dataframe()
+def get_frames_count(id, df) :
     res = (df.loc[id,:]).tolist()
     return res
+
+def get_doc_bigrams(id, df) :
+    res = (df.loc[id,:]).tolist()
+    return res
+    
+def intersect(L,M) :
+    return [elt for elt in list(L) if elt in list(M)]
+    return [(i,word) for i,word in enumerate(list(L)) if word in M]
 
 # Computes the number of similar frames. Finds the similar frames for two articles.
 def common_frames(L,M):
@@ -46,20 +58,26 @@ def common_frames(L,M):
     return sum([L[i]>0 and M[i]>0 for i in range(len(L))])
 
 """ Given a document id, returns the topn most similar documents in terms of
-the cosine similarity between their respective frame counts. The vectors are normalized
-beforehand. """
+cosine similarity between their respective frame counts. The vectors are normalized beforehand. """
 
-def most_similar(id1, topn = 50, df = pd.DataFrame()) :
-    if len(df) == 0 :
-        df = load_dataframe()
-
+def most_similar(id1, df, topn = 50) :
     L = get_frames_count(id1, df)
-
     def score(id2) :                   # inner function, returns the similarity between id1 and id2
         M = get_frames_count(id2, df)
         return normalized_cosine_sim(L,M)
-
+        
     docs = [df.index[i] for i in range(len(df)) if df.index[i]!= id1]
     most_sim_docs = sorted(docs, key = score, reverse = True)[:topn]
-
+    return most_sim_docs
+    
+def most_similar_with_bigrams(id1, df, topn = 50) :
+    L = get_doc_bigrams(id1, df)
+    def score(id2) :
+        M = get_doc_bigrams(id2, df)
+        s = len(intersect(L,M))
+        if s > 0 :
+            print(s)
+        return s
+    docs = [df.index[i] for i in range(len(df)) if df.index[i]!= id1]
+    most_sim_docs = sorted(docs, key = score, reverse = True)[:topn]
     return most_sim_docs
